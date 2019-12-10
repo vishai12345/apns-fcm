04b78696e8ad78cdf41a21cfa631f07fc03af4ba
Vishal Chaturvedi <vishal.chaturvedi@gmail.com>
2019-12-10 09:16:12 +0530

    clean some code

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const sendAPNSNotifications = require("./apns").sendAPNSNotifications
 
 const sendNotification = (payload, platform) => {
   return platform === "fcm" ? 
-      sendFCMNotifications(payload) : 
-          (platform === "apns" ? sendAPNSNotifications(payload) : new Promise((_, reject) => reject("Error: Pass platform value")))
+    sendFCMNotifications(payload) : 
+      (platform === "apns" ? sendAPNSNotifications(payload) :
+        new Promise((_, reject) => reject("Error: Pass platform value")))
 }
 
 module.exports = sendNotification
